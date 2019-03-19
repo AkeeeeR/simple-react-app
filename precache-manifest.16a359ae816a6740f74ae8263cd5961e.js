cbf055197055a5448fc4ca74a4b6ff175e9aabd3
Arkadiusz Moroń <ak.er@wp.pl>
2019-03-19 11:03:10 +0100

    Updates

--- precache-manifest.16a359ae816a6740f74ae8263cd5961e.js
+++ precache-manifest.16a359ae816a6740f74ae8263cd5961e.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/simple-react-app/static/js/runtime~main.d5660f83.js"
   },
   {
-    "revision": "52283e5a04f290f0a916",
-    "url": "/simple-react-app/static/js/main.7b469983.chunk.js"
+    "revision": "1617c43abb35db9705e5",
+    "url": "/simple-react-app/static/js/main.addcc01c.chunk.js"
   },
   {
     "revision": "9122617a03067dc8d218",
     "url": "/simple-react-app/static/js/2.26ff2db8.chunk.js"
   },
   {
-    "revision": "52283e5a04f290f0a916",
+    "revision": "1617c43abb35db9705e5",
     "url": "/simple-react-app/static/css/main.c867eb38.chunk.css"
   },
   {
-    "revision": "d434b3da438f4614fc2d0f3d045b1b94",
+    "revision": "ead2d9259be4b8a56dbdfe8bd53f182f",
     "url": "/simple-react-app/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/simple-react-app/precache-manifest.bcf2a2272782d86104bd659a2c7dd1df.js"
+  "/simple-react-app/precache-manifest.16a359ae816a6740f74ae8263cd5961e.js"
 );
 
 workbox.clientsClaim();
