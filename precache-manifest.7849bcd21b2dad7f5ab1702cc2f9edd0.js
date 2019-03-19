d1b22f99f18ffd8d01427628c4a02a8259f8d29a
Arkadiusz Moroń <ak.er@wp.pl>
2019-03-19 01:44:40 +0100

    Updates

---
+++ precache-manifest.7849bcd21b2dad7f5ab1702cc2f9edd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fd64f1acbc6de00f5ea",
+    "url": "/simple-react-app/static/js/runtime~main.d5660f83.js"
+  },
+  {
+    "revision": "31c54a7f492414465af4",
+    "url": "/simple-react-app/static/js/main.f4e6273f.chunk.js"
+  },
+  {
+    "revision": "9122617a03067dc8d218",
+    "url": "/simple-react-app/static/js/2.26ff2db8.chunk.js"
+  },
+  {
+    "revision": "31c54a7f492414465af4",
+    "url": "/simple-react-app/static/css/main.4a3afb56.chunk.css"
+  },
+  {
+    "revision": "6cf56a636515e6627fe61fdf1d3a5da3",
+    "url": "/simple-react-app/index.html"
+  }
+];
